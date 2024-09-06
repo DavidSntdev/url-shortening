e6f82f7cb95383e6268daf68da0d265194bada89
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-09-06 00:04:43 -0300

    Teste

--- components/login.tsx
+++ components/login.tsx
@@ -6,7 +6,9 @@ export default function Login() {
       <Button className="bg-transparent text-grayishViolet font-bold">
         Login
       </Button>
-      <Button className="bg-cyan rounded-full px-4 font-bold text-white">Sign Up</Button>
+      <Button className="bg-cyan rounded-full px-4 font-bold text-white">
+        Sign Up
+      </Button>
     </>
   );
 }
